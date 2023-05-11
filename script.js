760ea7a599056b6112cfdbd7e3d99301d6f5b880
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-05-11 12:19:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // your code here
+
+const url = document.getElementById('url');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+  let nam = document.getElementById('name').value;
+  let num = document.getElementById('year').value;
+ if (nam && num) {
+    url.innerText = `https://localhost:8080/?name=${nam}&year=${num}`;
+  } else if (nam) {
+    url.innerText = `https://localhost:8080/?name=${nam}`;
+  }
+	 else if (num) {
+    url.innerText = `https://localhost:8080/?year=${num}`;
+  }
+ else {
+    url.innerText = `https://localhost:8080/`;
+  }});
